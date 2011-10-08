69019564bb7a91312beca20227b6834a2909c19b
Anil Madhavapeddy <anil@recoil.org>
2011-10-08 21:03:09 +0100

    add Data.parse_c to read the data sections of the bytecode.
    Almost matches the bytecode executable reader, except for an extra 'int:0' value at the end of the unmarshalled bytecode which isnt present in the C one. Odd...

--- src/data.ml
+++ src/data.ml
@@ -67,3 +67,48 @@ let clean code orig_data =
 let export oc data =
   output_value oc data;
 ;;
+
+let print data =
+  let open Printf in let open Obj in
+  let ctr = ref 0 in
+  let out x = printf "%d: %s\n%!" !ctr x; incr ctr in
+  Array.iter (fun d ->
+    match is_block d, is_int d with
+    |true,false -> begin
+      match tag d with
+      |t when t = string_tag -> out (sprintf "string: %s" (Obj.magic d))
+      |t when t = int_tag -> out (sprintf "int: %d" (Obj.magic d))
+      |t when t = custom_tag -> out "custom"
+      |t when t = 0 -> out (sprintf "block: %d" (size d))
+      |_ -> out "XXX"
+     
+    end
+    |false,true -> out (sprintf "int:%d" (Obj.magic d))
+    |_ -> Printf.printf "unknown block\n%!"
+  ) data
+;;
+
+let parse_c ic =
+  let rec scan_until s = 
+    let line = input_line ic in
+    if line = s then () else scan_until s
+  in 
+  let rec scan_iter s1 s2 fn =
+    scan_until s1;
+    let rec loop () =
+      let l2 = input_line ic in
+      if l2 = s2 then () else (fn l2; loop ())
+    in
+    loop ()
+  in
+  let buf = Buffer.create 128 in
+  scan_iter "static char caml_data[] = {" "};"
+    (fun line ->
+      let bits = Str.(split (regexp_string ", ") line) in
+      List.iter (fun n ->
+        Buffer.add_char buf (Char.chr (int_of_string n))
+      ) bits
+    );
+  let (data : Obj.t array) = Marshal.from_string (Buffer.contents buf) 0 in
+  data
+
